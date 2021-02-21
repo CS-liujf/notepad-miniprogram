b2d7d8e834bfe4f04926af9e99c9873961833c56
CS-liujf <1756339062@qq.com>
2021-02-21 13:01:31 +0800

    更换回最早的状态

--- pages/notebook/notebook.js
+++ pages/notebook/notebook.js
@@ -1,5 +1,4 @@
 // pages/notebook/notebook.js
-const app = getApp()
 const db=wx.cloud.database();
 const notes=db.collection('notes');
 
@@ -15,9 +14,6 @@ Page({
     isMax:false,
     proh:false,
     openid:'',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   add:function(e){
     console.log(e);
@@ -70,33 +66,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-
     wx.cloud.callFunction({
       name:'getOpenId'
     })
